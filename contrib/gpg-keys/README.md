1c8f1375b520f1da68a6acf506e65215725d8272
scribenetwork <38965380+scribenetwork@users.noreply.github.com>
2020-08-13 07:58:58 +0000

    refs https://github.com/scribenetwork/scribe/issues/1

---
+++ contrib/gpg-keys/README.md
@@ -0,0 +1,12 @@
+# GPG-keys
+
+### Checksums:
+```
+MD5 (lorem.gpg) = 248f91d55de243f5bde7ddf1f9c406b1
+```
+
+### Authorized credentials:
+```
+Discord: Lorem#4860
+Email: dev@scribe.network
+```
--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -195,8 +195,9 @@ public:
         // Scribe BIP32 prvkeys start with 'xprv' (Bitcoin defaults)
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
-        // Scribe BIP44 coin type is '5'
-        nExtCoinType = 5;
+        // Scribe BIP44 coin type was '5', requested '545' for SCRIBE at SLIP44
+        // see https://github.com/satoshilabs/slips/pull/969
+        nExtCoinType = 545;
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
